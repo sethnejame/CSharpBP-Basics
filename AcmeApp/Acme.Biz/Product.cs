0d0acb1c6a9663e0333cf2478dc581f9c32ba105
Seth NeJame <seth@worldfavor.com>
2019-12-17 16:17:57 +0100

    Adding Product class w/ unit tests

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -5,8 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Acme.Biz
-{
-    class Product
+{   
+    
+    /// <summary>
+    /// Manages products carried in inventory
+    /// </summary>
+    public class Product
     {
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        private int productId;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+        public string SayHello()
+        {
+            return "Hello " + ProductName +
+                   " (" + ProductId + "): " +
+                   Description;
+        }
+
     }
 }
