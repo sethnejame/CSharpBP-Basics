c18c54aa1c5b3697061e91bf51c64348a378ade7
Seth <sethnejame@gmail.com>
2020-01-09 06:58:27 +0100

    Adding ToString override for better class readability

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using Acme.Common;
@@ -99,5 +100,9 @@ namespace Acme.Biz
                 AvailabilityDate?.ToShortDateString();
         }
 
+        public override string ToString()
+        {
+            return this.ProductName + " (" + this.ProductId + ") ";
+        }
     }
 }
